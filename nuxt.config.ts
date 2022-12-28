da5f8d280ed134498b923aa6e6c0c48f0e812976
Hoang Nguyen <hoangnguyen@Hoangs-MacBook-Pro.local>
2022-12-28 17:16:24 +0700

    initial prismic

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  buildModules: [
+    '@nuxtjs/prismic'
+  ],
+  prismic: {
+    endpoint: 'https://<REPOSITORY>.cdn.prismic.io/api/v2',
+    modern: true
+    /* see configuration for more */
+  },
   css: ["~/assets/css/tailwind.css"],
   postcss: {
     plugins: {
