09fb36623186923f2ab4dec147a009a4420bd9d2
Hoang Nguyen <hoangnguyen@Hoangs-MacBook-Pro.local>
2023-01-10 07:11:10 +0700

    update fav icon and image contact us

--- app.vue
+++ app.vue
@@ -431,7 +431,7 @@
         <!-- CTA Section -->
         <div class="relative bg-gray-900">
           <div class="relative h-56 bg-indigo-600 sm:h-72 md:absolute md:left-0 md:h-full md:w-1/2">
-            <img class="h-full w-full object-cover" src="https://images.unsplash.com/photo-1525130413817-d45c1d127c42?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1920&q=60&sat=-100" alt="" />
+            <img class="h-full w-full object-cover" src="/contact-us-team.png" alt="Contact us Bitatech" />
             <div aria-hidden="true" class="absolute inset-0 bg-gradient-to-r from-yellow-200 to-orange-400 mix-blend-multiply" />
           </div>
           <div class="relative mx-auto max-w-md px-4 py-12 sm:max-w-7xl sm:px-6 sm:py-20 md:py-28 lg:px-8 lg:py-32">
