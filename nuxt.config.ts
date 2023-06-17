89cc7e6d393b0c0a2601c78e097e755673c34f4d
Hoang Nguyen <hoangnguyen@Hoangs-MacBook-Pro-2.local>
2023-06-18 00:37:32 +0700

    init prismic

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,26 +2,29 @@
 export default defineNuxtConfig({
   app: {
     head: {
-      charset: 'utf-16',
-      viewport: 'width=500, initial-scale=1',
-      title: 'Bitatech Technology Services Company Limited',
+      charset: "utf-16",
+      viewport: "width=500, initial-scale=1",
+      title: "Bitatech Technology Services Company Limited",
       meta: [
         // <meta name="description" content="My amazing site">
-        { name: 'description', content: 'We partner with founders, startups, and enterprises to design and create MVP, products, and services following industry best practices.' }
+        {
+          name: "description",
+          content:
+            "We partner with founders, startups, and enterprises to design and create MVP, products, and services following industry best practices.",
+        },
       ],
-    }
+    },
   },
   modules: [
-    '@nuxt/image-edge',
-    '@nuxtjs/tailwindcss',
-    'nuxt-swiper'
+    "@nuxt/image-edge",
+    "@nuxtjs/tailwindcss",
+    "nuxt-swiper",
+    "@nuxtjs/prismic",
   ],
-  buildModules: [
-    '@nuxtjs/prismic',
-  ],
+  buildModules: ["@nuxtjs/prismic"],
   prismic: {
-    endpoint: 'https://<REPOSITORY>.cdn.prismic.io/api/v2',
-    modern: true
+    endpoint: "bitatech-marketing",
+    modern: true,
     /* see configuration for more */
   },
   css: ["~/assets/css/tailwind.css"],
@@ -32,10 +35,9 @@ export default defineNuxtConfig({
     },
   },
   image: {},
-  vue: {  
+  vue: {
     compilerOptions: {
-      isCustomElement: (tag) => ['tel'].includes(tag),
+      isCustomElement: (tag) => ["tel"].includes(tag),
     },
-  }
-})
-
+  },
+});
