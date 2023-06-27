d34355924bf3395acc78d22cdd15ae3af0d492f3
Hoang Nguyen <hoangnguyen@Hoangs-MacBook-Pro-2.local>
2023-06-27 11:35:41 +0700

    update testimonial

--- components/homePage/Testimonial.vue
+++ components/homePage/Testimonial.vue
@@ -34,81 +34,83 @@ const testimonials = [
 <template>
   <!-- Testimonial section -->
   <section class="bg-gray-500">
-    <Swiper
-      :modules="[SwiperAutoplay, SwiperPagination]"
-      :loop="true"
-      :autoplay="{
-        delay: 4000,
-        disableOnInteraction: false,
-        reverseDirection: true
-      }"
-      :breakpoints="{
-        320: {
-          slidesPerView: 'auto',
-        },
-        1024: {
-          slidesPerView: 2,
-        },
-      }"
-      :pagination="{
-        type: 'bullets',
-        clickable: false,
-      }"
-    >
-      <SwiperSlide
-        v-for="testimonial in testimonials"
-        :key="testimonial.name"
-        class="custom-swiper"
+    <div class="mx-auto max-w-7xl">
+      <Swiper
+        :modules="[SwiperAutoplay, SwiperPagination]"
+        :loop="true"
+        :autoplay="{
+          delay: 4000,
+          disableOnInteraction: false,
+          reverseDirection: true
+        }"
+        :breakpoints="{
+          320: {
+            slidesPerView: 'auto',
+          },
+          1024: {
+            slidesPerView: 2,
+          },
+        }"
+        :pagination="{
+          type: 'bullets',
+          clickable: false,
+        }"
       >
-        <div
-          class="py-12 px-6 md:flex md:flex-col md:border-r md:border-gray-500 md:py-16 lg:px-16"
+        <SwiperSlide
+          v-for="testimonial in testimonials"
+          :key="testimonial.name"
+          class="custom-swiper"
         >
-          <div class="md:flex-shrink-0">
-            <img
-              class="h-12 w-12 rounded-full lg:h-16 lg:w-16"
-              :src="testimonial.companyLogo"
-              :alt="testimonial.companyName"
-            />
-          </div>
-          <blockquote class="mt-6 md:flex md:flex-grow md:flex-col">
-            <div class="relative text-lg font-medium text-white md:flex-grow">
-              <svg
-                class="absolute top-0 left-0 h-8 w-8 -translate-x-3 -translate-y-2 transform text-gray-400"
-                fill="currentColor"
-                viewBox="0 0 32 32"
-                aria-hidden="true"
-              >
-                <path
-                  d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z"
-                />
-              </svg>
-              <p class="relative">
-                {{ testimonial.content }}
-              </p>
+          <div
+            class="py-12 px-6 md:flex md:flex-col md:border-r md:border-gray-500 md:py-16 lg:px-16"
+          >
+            <div class="md:flex-shrink-0">
+              <img
+                class="h-12 w-12 rounded-full lg:h-16 lg:w-16"
+                :src="testimonial.companyLogo"
+                :alt="testimonial.companyName"
+              />
             </div>
-            <footer class="mt-8">
-              <div class="flex items-start">
-                <div
-                  class="inline-flex flex-shrink-0 rounded-full border-2 border-white"
+            <blockquote class="mt-6 md:flex md:flex-grow md:flex-col">
+              <div class="relative text-lg font-medium text-white md:flex-grow">
+                <svg
+                  class="absolute top-0 left-0 h-8 w-8 -translate-x-3 -translate-y-2 transform text-gray-400"
+                  fill="currentColor"
+                  viewBox="0 0 32 32"
+                  aria-hidden="true"
                 >
-                  <img
-                    class="object-cover h-16 w-16 rounded-full"
-                    :src="testimonial.avatar"
-                    :alt="testimonial.name"
+                  <path
+                    d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z"
                   />
-                </div>
-                <div class="ml-4">
-                  <div class="text-base font-medium text-white">{{ testimonial.name }}</div>
-                  <div class="text-base font-medium text-indigo-200">
-                    {{ testimonial.position }}
+                </svg>
+                <p class="relative">
+                  {{ testimonial.content }}
+                </p>
+              </div>
+              <footer class="mt-8">
+                <div class="flex items-start">
+                  <div
+                    class="inline-flex flex-shrink-0 rounded-full border-2 border-white"
+                  >
+                    <img
+                      class="object-cover h-16 w-16 rounded-full"
+                      :src="testimonial.avatar"
+                      :alt="testimonial.name"
+                    />
+                  </div>
+                  <div class="ml-4">
+                    <div class="text-base font-medium text-white">{{ testimonial.name }}</div>
+                    <div class="text-base font-medium text-indigo-200">
+                      {{ testimonial.position }}
+                    </div>
                   </div>
                 </div>
-              </div>
-            </footer>
-          </blockquote>
-        </div>
-      </SwiperSlide>
-    </Swiper>
+              </footer>
+            </blockquote>
+          </div>
+        </SwiperSlide>
+      </Swiper>
+    </div>
   </section>
 </template>
 
